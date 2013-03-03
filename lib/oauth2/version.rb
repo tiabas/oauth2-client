0d317f75d14393b0163cda2d78a5c41bb931703a
Kevin Mutyaba <tiabasnk@gmail.com>
2013-03-03 00:21:34 -0800

    Bumped version to 1.1.2

--- lib/oauth2/version.rb
+++ lib/oauth2/version.rb
@@ -2,7 +2,7 @@ module OAuth2
   class Version
     MAJOR = 1 unless defined? OAuth2::Version::MAJOR
     MINOR = 1 unless defined? OAuth2::Version::MINOR
-    PATCH = 1 unless defined? OAuth2::Version::PATCH
+    PATCH = 2 unless defined? OAuth2::Version::PATCH
 
     def self.to_s
       [MAJOR, MINOR, PATCH].compact.join('.')
